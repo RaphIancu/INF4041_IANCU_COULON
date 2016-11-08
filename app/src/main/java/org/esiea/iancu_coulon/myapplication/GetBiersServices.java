4b2b6434837d629d09423510b9d7c71abe38906d
iancu <rififi3@hotmail.fr>
2016-11-08 16:38:09 +0100

    service avec JSON

--- app/src/main/java/org/esiea/iancu_coulon/myapplication/GetBiersServices.java
+++ app/src/main/java/org/esiea/iancu_coulon/myapplication/GetBiersServices.java
@@ -1,9 +1,24 @@
 package org.esiea.iancu_coulon.myapplication;
 
 import android.app.IntentService;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.Context;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import static org.esiea.iancu_coulon.myapplication.MainActivity.BIERS_UPDATE;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -16,7 +31,6 @@ public class GetBiersServices extends IntentService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_BIERS = "org.esiea.iancu_coulon.myapplication.action.BIERS";
-
 
     public GetBiersServices() {
         super("GetBiersServices");
@@ -35,8 +49,6 @@ public class GetBiersServices extends IntentService {
         context.startService(intent);
     }
 
-
-
     @Override
     protected void onHandleIntent(Intent intent) {
         handleActionBiers();
@@ -47,8 +59,39 @@ public class GetBiersServices extends IntentService {
      * parameters.
      */
     private void handleActionBiers() {
-        Log.i("erreur", "buggggggggggg");
+        Log.i("Log", "Thread service name:"+Thread.currentThread().getName());
+        URL url = null;
+        try{
+            url = new URL("http://binouze.fabrigli.fr/bieres.json");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if(HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(), new File(getCacheDir(), "bieres.json"));
+                Log.d("Log", "Bieres json download");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(BIERS_UPDATE));
+            }
+        } catch(MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-
+    private void copyInputStreamToFile(InputStream in, File file){
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0){
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/org/esiea/iancu_coulon/myapplication/MainActivity.java
+++ app/src/main/java/org/esiea/iancu_coulon/myapplication/MainActivity.java
@@ -1,8 +1,13 @@
 package org.esiea.iancu_coulon.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,6 +16,13 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String BIERS_UPDATE="com.octip.cours.inf4042_11.BIERS_UPDATE";
+
+    public class BierUpdate extends BroadcastReceiver {
+        public void onReceive(Context context, Intent intent){
+            Log.d("Log", getIntent().getAction());
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        IntentFilter intentFilter = new IntentFilter(BIERS_UPDATE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new BierUpdate(), intentFilter);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
